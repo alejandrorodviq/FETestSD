effc1bb492d4f943540fa1e42b1b0d2265c2aa5c
Alejandro Rodriguez <alejandrorodviq@gmail.com>
2021-04-22 15:15:24 -0600

    Final

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,1 +1,13 @@
 console.log('SD Frontend Test');
+
+function openNav() {
+    var x = document.getElementById("mobile-menu");
+    if (x.className === "inactive") {
+      x.className = "";
+      document.getElementById("menu-btn").src = "images/menu-close-btn.png";
+    } else {
+      x.className = "inactive";
+      document.getElementById("menu-btn").src = "images/menu-btn.png";
+    }
+  }
+
